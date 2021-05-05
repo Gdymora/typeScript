aea7a13d3dd62fa7829eead281af6c5f48e6580a
Alexandr <alekspetrofint@gmail.com>
2021-05-05 12:25:07 +0300

    env, nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 
-const app = express();
-const port = 3000;
+require("dotenv").config();
+declare var process: {
+  env: {
+    PORT: number; 
+    HOST: string;
+  };
+};
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Start progect express!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, host, () => {
+  console.log(`Example - app listening at ${host}:${port}`);
+});
