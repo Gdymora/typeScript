68f5a5839ec6b42ca266acf8edb9b26d00605a5f
Alexandr <alekspetrofint@gmail.com>
2021-05-07 12:24:30 +0300

    fetch axios DOS atak

--- src/module/axios-module.ts
+++ src/module/axios-module.ts
@@ -7,13 +7,15 @@ export async function testAxios() {
     const url = "https://jsonplaceholder.typicode.com/posts/1";
 
     try {
-        const response = await axios.get(url,{
-            timeout: 1000
+        const response = await axios.get(url, {
+          maxContentLength: 2000, timeout: 1000
         });
         const data = response.data;
         console.log(data);
         return data;
     } catch (error) {
         console.log(error);
+        error.request.res.destroy(); // нівелює вразливість DOS атак від переповнення maxContentLength (виправлення # 1098)
+         
     }
 }
